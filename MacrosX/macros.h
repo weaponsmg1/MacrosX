2a5de00f3bce5171e336878c200ff879ca0e2245
weapon_smg1 <114591031+weaponsmg1@users.noreply.github.com>
2025-11-10 10:29:26 +0200

    Update macros.h

--- MacrosX/macros.h
+++ MacrosX/macros.h
@@ -4,9 +4,13 @@ class Macros
 {
 public:
 	void MoveMouse();
+	void BunnyHop();
+	void AutoPistol();
 };
 
 extern bool enable;
+extern bool bhop;
+extern bool autopistol;
 extern int movex;
 extern int movey;
 
