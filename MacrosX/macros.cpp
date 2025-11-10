f2a4d3c2842f71138197d87b9e3159f2689ee669
weapon_smg1 <114591031+weaponsmg1@users.noreply.github.com>
2025-11-10 10:29:55 +0200

    Update macros.cpp
    Added bunny hop and auto pistol functionalities to the macros.

--- MacrosX/macros.cpp
+++ MacrosX/macros.cpp
@@ -2,6 +2,8 @@
 #include <windows.h>
 
 bool enable = false;
+bool bhop = false;
+bool autopistol = false;
 int movex = 0;
 int movey = 3;
 
@@ -18,3 +20,22 @@ void Macros::MoveMouse()
 	}
 }
 
+void Macros::BunnyHop()
+{
+	if (bhop && GetAsyncKeyState(VK_SPACE) & 0x8000)
+	{
+		keybd_event(VK_SPACE, 0, 0, 0);            
+		keybd_event(VK_SPACE, 0, KEYEVENTF_KEYUP, 0); 
+		Sleep(20);
+	}
+}
+
+void Macros::AutoPistol()
+{
+	if (autopistol && GetAsyncKeyState(VK_XBUTTON1) & 0x8000)
+	{
+		mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+		mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+		Sleep(10);
+	}
+}
